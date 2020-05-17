256df99eee3e8275a8225607ad9fe34272be0f64
crisj1985 <crisj1985@hotmail.com>
2020-05-17 13:29:04 -0500

    Header: Fuente

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,8 @@ const Header = () => <Text style={styles.encabezado}>Criptomonedas</Text>
 
 const styles = StyleSheet.create({
   encabezado: {
-    paddingTop: Platform.OS ==='ios' ? 50 : 10,
+    paddingTop: Platform.OS === 'ios' ? 50 : 10,
+    fontFamily: 'Lato-Black'
   },
 })
 
