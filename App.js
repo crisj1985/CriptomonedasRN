50494191e2ba83b540fb5197aa59ee318a4b9a46
crisj1985 <crisj1985@hotmail.com>
2020-05-19 23:44:28 -0500

    Formulario:Picker, State moneda

--- App.js
+++ App.js
@@ -8,22 +8,31 @@ import {
   Image,
 } from 'react-native';
 import Header from './components/Header'
+import Formulario from './components/Formulario'
 
 const App = () => {
   return (
     <>
-      <Header/>
-      <Image source={require("./assets/img/cryptomonedas.png")} style={styles.image}/>
+      <Header />
+
+      <Image source={require('./assets/img/cryptomonedas.png')} style={styles.image} />
+
+      <View style = {styles.contenido}>
+        <Formulario />
+      </View>
     </>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
-  image:{
-    width:"100%",
-    height:140,
-    marginHorizontal:"2.5%"
+  image: {
+    width: '100%',
+    height: 140,
+    marginHorizontal: '2.5%',
+  },
+  contenido:{
+    marginHorizontal:'2.5%'
   }
-});
+})
 
 export default App;
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,1 +1,44 @@
 
+import React, {useState}from  "react";
+import {StyleSheet,Text, View} from 'react-native';
+import { Picker } from '@react-native-community/picker';
+
+
+const Formulario  = () =>{
+
+    const [moneda, setMoneda] = useState('');
+    const [criptomoneda, setCriptomoneda] = useState('')
+
+    const obtenerMoneda = (moneda) => {
+    setMoneda(moneda);
+}
+
+return (
+  <View>
+    <Text style={styles.label}>Moneda</Text>
+    <Picker
+    onValueChange = { moneda => obtenerMoneda(moneda)}
+    selectedValue = {moneda}
+    >
+      <Picker.Item label="-Seleccione-" value="" />
+      <Picker.Item label="Dolar de USA" value="USD" />
+      <Picker.Item label="Peso Mexicano" value="MXN" />
+      <Picker.Item label="Euro" value="EUR" />
+      <Picker.Item label="Libra Esterlina" value="GBP" />
+    </Picker>
+    <Text style={styles.label}>CriptoMoneda</Text>
+  </View>
+)
+}
+
+const styles = StyleSheet.create({
+label:{
+    fontFamily:'Lato-Black',
+    textTransform:'uppercase',
+    fontSize:22,
+    marginVertical:20
+}
+});
+export default Formulario
+
+
