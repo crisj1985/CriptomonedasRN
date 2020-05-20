0eacb21756737f8921761dc48a391334c7f74f33
crisj1985 <crisj1985@hotmail.com>
2020-05-20 00:39:00 -0500

    Formulario:Picker Criptomoneda

--- components/Formulario.js
+++ components/Formulario.js
@@ -9,26 +9,30 @@ const Formulario  = () =>{
 
     const [moneda, setMoneda] = useState('');
     const [criptomoneda, setCriptomoneda] = useState('');
+    const [criptomonedas, setCriptomonedas] = useState([])
     
     useEffect(() => {
         const ConsultarApi = async () => {
                 const url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
                 const resultado = await axios.get(url);
-
+                console.log(resultado);
+                
+                setCriptomonedas(resultado.data.Data)    
         }
+        ConsultarApi();
     },[])
 
     const obtenerMoneda = (moneda) => {
     setMoneda(moneda);
+    }
+     const obtenerCriptomoneda = (cripto) => {
+    setCriptomoneda(cripto);
 }
 
 return (
   <View>
     <Text style={styles.label}>Moneda</Text>
-    <Picker
-    onValueChange = { moneda => obtenerMoneda(moneda)}
-    selectedValue = {moneda}
-    >
+    <Picker onValueChange={(moneda) => obtenerMoneda(moneda)} selectedValue={moneda} itemStyle={{ height: 120 }}>
       <Picker.Item label="-Seleccione-" value="" />
       <Picker.Item label="Dolar de USA" value="USD" />
       <Picker.Item label="Peso Mexicano" value="MXN" />
@@ -36,6 +40,12 @@ return (
       <Picker.Item label="Libra Esterlina" value="GBP" />
     </Picker>
     <Text style={styles.label}>CriptoMoneda</Text>
+    <Picker   onValueChange = {(cripto)=>obtenerCriptomoneda(cripto)} selectedValue={criptomoneda} itemStyle={{ height: 120 }}>
+      <Picker.Item label="-Seleccione-" value="" />
+      {criptomonedas.map((cripto) => (
+        <Picker.Item key={cripto.CoinInfo.Id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name} />
+      ))}
+    </Picker>
   </View>
 )
 }
