29d471dba807b4392ca534cca24e514d6ddf257f
crisj1985 <crisj1985@hotmail.com>
2020-05-24 22:57:28 -0500

    Button Cotizar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -7,18 +7,43 @@ import {
   Text,
   Image,
 } from 'react-native';
-import Header from './components/Header'
-import Formulario from './components/Formulario'
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import axios from 'axios';
 
 const App = () => {
+
+      const [moneda, setMoneda] = useState('');
+      const [criptomoneda, setCriptomoneda] = useState('');
+      const [consultarAPI, setConsultarAPI] = useState(false);
+
+      useEffect(() => {
+       const cotizarCriptomoneda = async () => {
+           if (consultarAPI) {
+             const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+             console.log(url);
+             const resultado = await axios.get(url)
+             console.log(resultado.data.DISPLAY);
+             setConsultarAPI(false);
+           }
+        }
+        cotizarCriptomoneda();
+      }, [consultarAPI])
+
   return (
     <>
       <Header />
 
       <Image source={require('./assets/img/cryptomonedas.png')} style={styles.image} />
 
-      <View style = {styles.contenido}>
-        <Formulario />
+      <View style={styles.contenido}>
+        <Formulario
+          moneda={moneda}
+          criptomoneda={criptomoneda}
+          setMoneda={setMoneda}
+          setCriptomoneda={setCriptomoneda}
+          setConsultarAPI={setConsultarAPI}
+        />
       </View>
     </>
   )
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,53 +1,67 @@
 
 import React, {useState, useEffect}from  "react";
-import {StyleSheet,Text, View} from 'react-native';
+import {StyleSheet,Text, View, TouchableHighlight, Alert} from 'react-native';
 import { Picker } from '@react-native-community/picker';
 import axios from 'axios';
 
 
-const Formulario  = () =>{
+const Formulario = ({ moneda, criptomoneda, setMoneda, setCriptomoneda, setConsultarAPI }) => {
+  const [criptomonedas, setCriptomonedas] = useState([])
 
-    const [moneda, setMoneda] = useState('');
-    const [criptomoneda, setCriptomoneda] = useState('');
-    const [criptomonedas, setCriptomonedas] = useState([])
-    
-    useEffect(() => {
-        const ConsultarApi = async () => {
-                const url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD';
-                const resultado = await axios.get(url);
-                console.log(resultado);
-                
-                setCriptomonedas(resultado.data.Data)    
-        }
-        ConsultarApi();
-    },[])
+  useEffect(() => {
+    const ConsultarApi = async () => {
+      const url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD'
+      const resultado = await axios.get(url)
+      console.log(resultado)
 
-    const obtenerMoneda = (moneda) => {
-    setMoneda(moneda);
+      setCriptomonedas(resultado.data.Data)
     }
-     const obtenerCriptomoneda = (cripto) => {
-    setCriptomoneda(cripto);
-}
+    ConsultarApi()
+  }, [])
 
-return (
-  <View>
-    <Text style={styles.label}>Moneda</Text>
-    <Picker onValueChange={(moneda) => obtenerMoneda(moneda)} selectedValue={moneda} itemStyle={{ height: 120 }}>
-      <Picker.Item label="-Seleccione-" value="" />
-      <Picker.Item label="Dolar de USA" value="USD" />
-      <Picker.Item label="Peso Mexicano" value="MXN" />
-      <Picker.Item label="Euro" value="EUR" />
-      <Picker.Item label="Libra Esterlina" value="GBP" />
-    </Picker>
-    <Text style={styles.label}>CriptoMoneda</Text>
-    <Picker   onValueChange = {(cripto)=>obtenerCriptomoneda(cripto)} selectedValue={criptomoneda} itemStyle={{ height: 120 }}>
-      <Picker.Item label="-Seleccione-" value="" />
-      {criptomonedas.map((cripto) => (
-        <Picker.Item key={cripto.CoinInfo.Id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name} />
-      ))}
-    </Picker>
-  </View>
-)
+  const obtenerMoneda = (moneda) => {
+    setMoneda(moneda)
+  }
+  const obtenerCriptomoneda = (cripto) => {
+    setCriptomoneda(cripto)
+  }
+  const cotizarPrecio = () => {
+    if (moneda === '' || criptomoneda === '') {
+      mostrarAlert()
+      return
+    }
+    setConsultarAPI(true);
+  }
+  const mostrarAlert = () => {
+    Alert.alert('Error...', 'Ambos campos son obligatorios', [{ text: 'OK' }])
+  }
+
+  return (
+    <View>
+      <Text style={styles.label}>Moneda</Text>
+      <Picker onValueChange={(moneda) => obtenerMoneda(moneda)} selectedValue={moneda} itemStyle={{ height: 120 }}>
+        <Picker.Item label="-Seleccione-" value="" />
+        <Picker.Item label="Dolar de USA" value="USD" />
+        <Picker.Item label="Peso Mexicano" value="MXN" />
+        <Picker.Item label="Euro" value="EUR" />
+        <Picker.Item label="Libra Esterlina" value="GBP" />
+      </Picker>
+      <Text style={styles.label}>CriptoMoneda</Text>
+      <Picker
+        onValueChange={(cripto) => obtenerCriptomoneda(cripto)}
+        selectedValue={criptomoneda}
+        itemStyle={{ height: 120 }}
+      >
+        <Picker.Item label="-Seleccione-" value="" />
+        {criptomonedas.map((cripto) => (
+          <Picker.Item key={cripto.CoinInfo.Id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name} />
+        ))}
+      </Picker>
+      <TouchableHighlight style={styles.btnCotizar} onPress={() => cotizarPrecio()}>
+        <Text style={styles.texttoCotizar}>Cotizar</Text>
+      </TouchableHighlight>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -56,7 +70,20 @@ label:{
     textTransform:'uppercase',
     fontSize:22,
     marginVertical:20
+},
+btnCotizar:{
+backgroundColor:"#5E49E2",
+padding:10,
+marginTop:20
+},
+texttoCotizar:{
+  color:"#FFF",
+  fontSize:18,
+  fontFamily:'Lato-Black',
+  textTransform:'uppercase',
+  textAlign:'center'
 }
+
 });
 export default Formulario
 
